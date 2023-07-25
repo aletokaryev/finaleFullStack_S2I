40812979db2aea3534d606869317906d69a32d28
Alessandro Bulyakhovska Tokaryev <alessandrotokaryev060@gmail.com>
2023-07-25 14:05:37 +0200

    Fix URL

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API: 'https://expenses-backend-6bng.onrender.com'
+  API: 'https://expenses-backend-6bng.onrender.com/api'
   // API: 'http://localhost:3000/api'
 };
